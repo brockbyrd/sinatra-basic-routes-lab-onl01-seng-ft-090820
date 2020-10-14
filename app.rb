18989c95f12e84ad96c08956236cf9a0720e3a4b
Brock Byrd <brockbyrdd@gmail.com>
2020-10-14 20:56:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Brock Byrd"
+  end
+
+  get '/hometown' do
+    "My hometown is Long Beach"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 100 Grandkids by Mac Miller"
+  end
 end
