83d3527b7bb817e755dde8dccd1706d389c7d848
Brock Byrd <brockbyrdd@gmail.com>
2020-10-14 20:56:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Brock Byrd"
   end
+
+  get '/hometown' do
+    "My hometown is Long Beach"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 100 Grandkids by Mac Miller"
+  end
 end
